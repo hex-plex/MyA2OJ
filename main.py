7246b23f4188336781d691e7007743f7ab92344b
hex-plex <sendhilsom7@gmail.com>
2020-04-29 22:15:35 +0530

    Plans

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class MyA2OJ:
     Profile = None
     def __init__(self):
         pass
+
     def assignName(self,handle):
         try:
             self.User=CSA(handle)
@@ -16,12 +17,36 @@ class MyA2OJ:
             self.User=None
             self.Profile=None
             return False
-    def current():
-        if User is not None:
-            return check(getData,User)
+
+    def current(self):
+        if self.User is not None:
+            return check(getData,self.User)#return a json file with link
         else:
             return None
             
+    def fetch(self,json):
+        pass #current progress given problem
+
+    def __repr__():
+        pass #anything
+
+    def __call__():
+        pass #return something important maybe boolean maybe tags
+
+    def refresh():
+        pass #update User
+    ### These are after addition of SQL so Later
+    def skip():
+        pass #to be able to mark a question done when not done 
+    def search():
+        pass #search editorial of json file google
+
+    def Add():
+        pass #add a new question or set of questions with priority
+    def skipped():
+        pass #This is set of questions which is skipped and sorted based on difficult
+        # in reverse or based on recently skipped should be better
         
+    
         
-    check(getData("ladder.html"))
+    #check(getData("ladder.html"))
