a7e65d52d8d4edc0dc6ccd35d087d96a40b75512
hex-plex <sendhilsom7@gmail.com>
2020-05-11 21:24:30 +0530

    Files are not ready

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,36 @@
 from flask import render_template, flash, redirect, url_for,request
 from app import app
+from flask_login import current_user,login_user,logout_user,login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html',user=user)
+    return render_template('base.html')
+
+@app.route('/signup',methods=['GET'])
+def signUp():
+    return render_template('signup.html')
+
+
+@app.route('/signup',methods=['POST'])
+def signUp():
+    
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template
+
+@app.route('/switch')
+def switch():
+    logout_user()
+    return render_template('login.html')
+
+@login_required
+@app.route('/info')
+def info():
+    return render_template('info.html')
