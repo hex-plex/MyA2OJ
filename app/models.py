0a1b2d35a5abcc21ff0692080b9f6e99f391fc36
hex-plex <sendhilsom7@gmail.com>
2020-06-01 01:17:13 +0530

    Models

--- app/models.py
+++ app/models.py
@@ -11,21 +11,44 @@ def load_user(id):
 class Submission(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     verdict = db.Column(db.String(64))
-    User_id = db.Column(db.Integer, db.ForeignKey('User.id'),
+    index = db.Column(db.String(5))
+    contestId = db.Column(db.Integer)
+    pname = db.Column(db.String(64),unique=True)
+    timeCons = db.Column(db.Integer)
+    memoryCons = db.Column(db.Integer)
+    passedTestCount = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)
     user = db.relationship('User',
-        backref=db.backref('Submissions', lazy=True))
+        backref=db.backref('subs', lazy=True))
+    
     def __init__(self,jsonin):
-        pass
+        self.id = jsonin['id']
+        self.index = jsonin['problem']['index']
+        self.pname = jsonin['problem']['name']
+        self.contestId = jsonin['problem']['contestId']  ## It might be important to be certain from which part is this information is to be taken
+        ## As it would be that the person would have solved the question in a competition and not directly in codeforces hence its to test if
+        ## Author or problem is to be used in fetching contestid
+        self.verdict = jsonin['verdict']
+        self.timeCons = jsonin['timeConsumedMillis']
+        self.memoryCons = jsonin['memoryConsumedBytes']
+        self.passedTestCount = jsonin['passedTestCount']
+
+    def __repr__(self):
+        return '<Submission-'+self.verdict+'-'+str(self.id)+'>'
 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key=True)
     handle = db.Column(db.String(64),index = True, unique=True)
+
+    def reg(self):
+        jsonin={}
+        self.subs.append(Submission(jsonin))
     
     def __repr__(self):
         return '<User-'+str(self.handle)+'>'
 
 
 
-    ## THis should save the file
